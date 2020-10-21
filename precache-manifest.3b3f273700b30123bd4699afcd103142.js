52c15047f5d10f0b171d5d085ef38a1749ea70d1
loreleim <lorelei.miyamura@gmail.com>
2020-10-21 12:02:06 -0700

    Updates

---
+++ precache-manifest.3b3f273700b30123bd4699afcd103142.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f20d037c5fa04aac894073547df00e9",
+    "url": "/greenhaus/index.html"
+  },
+  {
+    "revision": "6f8ebcbc9ad4a0ebb93e",
+    "url": "/greenhaus/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "392587574465f700543c",
+    "url": "/greenhaus/static/js/2.606e815e.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/greenhaus/static/js/2.606e815e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f8ebcbc9ad4a0ebb93e",
+    "url": "/greenhaus/static/js/main.0f6ffd0f.chunk.js"
+  },
+  {
+    "revision": "965a9d2605d99a57e5b6",
+    "url": "/greenhaus/static/js/runtime-main.5ae55025.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/greenhaus/static/media/logo.5d5d9eef.svg"
+  }
+]);
