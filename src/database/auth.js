8524357b0e69f71d797e6864551a18badb95174a
loreleim <lorelei.miyamura@gmail.com>
2020-10-27 12:42:21 -0700

    gotta pass that null just in case

--- src/database/auth.js
+++ src/database/auth.js
@@ -4,7 +4,7 @@ import firebase from "./firebase";
 export const AuthContext = React.createContext();
 
 export const AuthProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState();
+  const [currentUser, setCurrentUser] = useState(null);
 
   useEffect(() => {
     firebase.auth().onAuthStateChanged(setCurrentUser);
